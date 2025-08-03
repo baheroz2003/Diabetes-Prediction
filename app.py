6e4ad11edc1f27f699fef8f3f1b15c1511ce98f9
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-08-04 00:32:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,25 @@
-
 import numpy as np
 import pickle
 import streamlit as st
 
-
 # loading the saved model
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
-
-
-
-# creating a function for Prediction
-
+# function for prediction
 def diabetes_prediction(input_data):
-    
-
-    # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
-
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
     prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
-
-    if (prediction[0] == 0):
-      return 'The person is not diabetic'
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
     else:
-      return 'The person is diabetic'
-  
-    
-  
+        return 'The person is diabetic'
+
 def main():
-    
-    
-    # giving a title
-    st.title('Diabetes Prediction Web App')
-    
-    
-    # getting the input data from the user
-    
-    
+    # Title
+    st.title('🧠 Smart Diabetes Prediction')
+
+    # Input form
     Pregnancies = st.text_input('Number of Pregnancies')
     Glucose = st.text_input('Glucose Level')
     BloodPressure = st.text_input('Blood Pressure value')
@@ -49,38 +28,24 @@ def main():
     BMI = st.text_input('BMI value')
     DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
     Age = st.text_input('Age of the Person')
-    
-    
-    # code for Prediction
+
     diagnosis = ''
-    
-    # creating a button for Prediction
-    
+
     if st.button('Diabetes Test Result'):
-        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-        
-        
+        diagnosis = diabetes_prediction([
+            Pregnancies, Glucose, BloodPressure, SkinThickness,
+            Insulin, BMI, DiabetesPedigreeFunction, Age
+        ])
+
     st.success(diagnosis)
-    
-    
-    
-    
-    
+
+    # Footer
+    st.markdown("""
+        <br><hr>
+        <div style='text-align: center; color: gray; font-size: 14px;'>
+            Developed by <b>Baheroz Zeya</b>
+        </div>
+    """, unsafe_allow_html=True)
+
 if __name__ == '__main__':
     main()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  
-    
-
-  
