71c01a3eec75d55f3d73d501753c02b9d5eacab5
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-08-04 00:12:06 +0530

    Update and rename Diabetes Prediction web app.py to app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open(r'D:\mlapp\trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
 
 
 
@@ -81,4 +82,5 @@ if __name__ == '__main__':
     
   
     
+
   
